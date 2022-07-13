0b054be5275a595f9593f23c7dadde59b3543e11
AiorNerok <person12ru@gmail.com>
2022-07-13 23:01:58 +0300

    add: interface & structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useReducer, useState } from 'react'
+
+enum ActionTypes {
+  add = 'add',
+  remove = 'remove',
+  update = 'update',
+  save = 'save',
+  load = 'load',
+}
+
+interface ITask {
+  id: number;
+  desc: string;
+  state: boolean;
+  date: Date
+}
+
+interface ITodoState {
+  list: ITask[]
+}
+
+interface IAction {
+  type: ActionTypes,
+  payload: any
+}
+
+let TodoState: ITodoState = {
+  list: [{
+    id: 1,
+    date: new Date(),
+    desc: 'test task',
+    state: false
+  }]
+}
+
+function reducer(state: ITodoState, { payload, type }: IAction): ITodoState {
+  switch (type) {
+    case ActionTypes.add:
+      return { ...state }
+    case ActionTypes.remove:
+      return { ...state }
+    case ActionTypes.update:
+      return { ...state }
+    case ActionTypes.save:
+      return { ...state }
+    case ActionTypes.load:
+      return { ...state }                    
+    default:
+      return state
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [state, dispatch] = useReducer(reducer, { ...TodoState })
+
+  console.log(state)
+  console.log(dispatch)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      work
     </div>
   )
 }
